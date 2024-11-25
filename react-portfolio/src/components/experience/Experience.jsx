ce8b5b3609dca210f2706973e84f3d69a087298a
Kena Tilahun <tilahunkena@gmail.com>
2024-11-24 18:10:57 -0600

    started Experience section will finish implementation later

--- react-portfolio/src/components/experience/Experience.jsx
+++ react-portfolio/src/components/experience/Experience.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import './Experience.css'
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
   return (
-    <section id='experience'>Experience</section>
+    <section id='experience'>
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className='container experience_container'>
+        <div className='experience_frontend'>
+          <h3>Frontend Development</h3>
+          <div className='experience_content'>
+            <article className='experience_details'>
+              <BsPatchCheckFill />
+              <h4></h4>
+              <small>Experienced</small>
+            </article>
+          </div>
+        </div>
+
+        <div className='experience_backend'>
+        <h3>Backend Development</h3>
+          <div className='experience_content'>
+            <article className='experience_details'>
+              <BsPatchCheckFill />
+              <h4></h4>
+              <small>Experienced</small>
+            </article>
+          </div>
+        </div>
+
+      </div>
+    </section>
   )
 }
 
