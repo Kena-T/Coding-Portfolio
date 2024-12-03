c0b72becc7511b5a7bb2a7c8d618a2ea4fb22a24
Kena Tilahun <tilahunkena@gmail.com>
2024-12-02 19:33:25 -0600

    Starting work on portfolio implmentation.

--- react-portfolio/src/components/portfolio/Portfolio.jsx
+++ react-portfolio/src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,19 @@ import './Portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="contaniner portfolio_container">
+        <article className='portfolio_item'>
+          <div className="portfolio_item-image"></div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/Kena-T/Website" className='btn'>Github</a>
+          <a href="https://github.com/Kena-T/Website" className='btn btn-primary'>Live Demo</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
